afe7a0a36afd2b22c69212c202695f74c9d56b53
meronSIS <167541754+meronsisay@users.noreply.github.com>
2025-06-09 17:06:45 +0300

    initial setup for profile

---
+++ internlink_frontend_flutter/lib/data/datasources/remote/profile_remote_data_source.dart
@@ -0,0 +1,19 @@
+// class ProfileRemoteDataSource {
+//   final Dio dio;
+
+//   ProfileRemoteDataSource(this.dio);
+
+//   Future<ProfileDto> getProfile() async {
+//     final res = await dio.get('/profile');
+//     return ProfileDto.fromJson(res.data);
+//   }
+
+//   Future<ProfileDto> updateProfile(int id, ProfileDto dto) async {
+//     final res = await dio.put('/profile/$id', data: dto.toJson());
+//     return ProfileDto.fromJson(res.data);
+//   }
+
+//   Future<void> deleteProfile(int id) async {
+//     await dio.delete('/profile/$id');
+//   }
+// }
