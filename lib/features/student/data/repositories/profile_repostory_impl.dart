91a83cd8621ba742a70c90da221f10b1aab0be77
meronSIS <167541754+meronsisay@users.noreply.github.com>
2025-06-10 11:56:39 +0300

    added usecases for the profile

--- lib/features/student/data/repositories/profile_repostory_impl.dart
+++ lib/features/student/data/repositories/profile_repostory_impl.dart
@@ -1,7 +1,7 @@
 import '../data_sources/remote/profile_remote_data_source.dart';
 import '../../data/models/response/profile_dto.dart';
 import '../../domain/entities/user_profile.dart';
-import '../../domain/repositories/user_profile.dart';
+import '../../domain/repositories/profile_repository.dart';
 
 class ProfileRepositoryImpl implements ProfileRepository {
   final ProfileRemoteDataSource remote;
